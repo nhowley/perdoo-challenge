1239136fa01e1bddadf7084078921f83bc3bcc7a
Natalie <natalie@emeraldstay.com>
2021-06-04 11:19:28 +0200

    add variable

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,33 +1,62 @@
 import React from 'react'
 import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
+import { gql } from 'graphql-tag'
 
 class ListPage extends React.Component {
-  render() {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+        characters:[]
+    }
+  }
+
+  componentDidMount() {
+    console.log("this.props", this.props.data.characters.results)
+    this.getCharacters()
+  }
+
+  getCharacters = ()=> {
+    console.log("props", this.props.data)
+    if(this.props.data.characters.info.next !== null){
+      console.log("ANOThER PAGE")
+    }
+  }
+
+  render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
     return (
       <div>
+        <h1>CHARACTERS GO HERE</h1>
         <div>
-          TODO: Display table with characters
+          {this.props.data.characters.results.map((character) =>
+            <div>{character.name}</div>
+          )}
         </div>
       </div>
     )
   }
 }
 
+
+
 const ALL_CHARS_QUERY = gql`
-  query {
-    characters(
-      page:0
-    ) {
-      info {
-        count
-      }
-      results {
-        name
-      }
+query Characters($page: Int = 0){
+  characters(
+    page:$page
+  ) {
+    info {
+      count
+      next
+    }
+    results {
+      name
     }
   }
+}
 `
 
 
-export default graphql(ALL_CHARS_QUERY)(ListPage)
+export default graphql(ALL_CHARS_QUERY)(ListPage);
